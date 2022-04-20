01770ff378eea104e5025dd610411d7cf20ba40a
Daniel Barenholz <d.barenholz@student.tue.nl>
2022-04-20 20:14:02 +0200

    Add betting

---
+++ problems/betting.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ld long double
+#define ll long long
+#define arr array
+
+int main()
+{
+  double a;
+  cin >> a;
+
+  cout << 100.0 / a << "\n";
+  cout << 100.0 / (100.0 - a) << "\n";
+  return 0;
+}
