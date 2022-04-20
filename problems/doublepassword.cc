6138084dd158e3a591fb8c75c40f432bd9bf91b9
Daniel Barenholz <d.barenholz@student.tue.nl>
2022-04-20 20:01:16 +0200

    Forgot to copy contents

--- problems/doublepassword.cc
+++ problems/doublepassword.cc
@@ -1,1 +1,25 @@
-doublepassword.cc
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ld long double
+#define ll long long
+#define arr array
+int main()
+{
+  string a, b;
+  cin >> a >> b;
+
+  int res = 1;
+
+  for (int i = 0; i < 4; i++)
+  {
+    if (a[i] != b[i])
+    {
+      res *= 2;
+    }
+  }
+
+  cout << res << "\n";
+  return 0;
+}
