09175f9a34b983f639ba3366e9a53fb05778e246
Daniel Barenholz <d.barenholz@student.tue.nl>
2022-04-20 16:08:02 +0200

    Add whichisgreater

---
+++ problems/whichisgreater.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ld long double
+#define ll long long
+#define arr array
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << (int) (a > b) << "\n";
+    return 0;
+}
