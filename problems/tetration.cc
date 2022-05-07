e243211217ec1bee660fb18ca4547d9931e70976
Daniel Barenholz <d.barenholz@student.tue.nl>
2022-05-07 11:03:03 +0200

    Add tetration

---
+++ problems/tetration.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ld long double
+#define ll long long
+#define vec vector
+#define arr array
+
+int main()
+{
+  ld x;
+  cin >> x;
+
+  cout << pow(x, (1 / x)) << "\n";
+  return 0;
+}
