1d34d9094963f61d1328b374967d7f0c54d905c5
Daniel Barenholz <hello@dbarenholz.com>
2024-03-18 16:53:02 +0100

    Add aaah.hs

---
+++ src/aaah/aaah.hs
@@ -0,0 +1,30 @@
+import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty ((:|)), (<|))
+import qualified Data.List.NonEmpty as NE
+
+main :: IO ()
+main = interact (formatOutput . solve . parseInput)
+
+-- helpers for parsing
+splitOn' :: Char -> String -> NonEmpty String
+splitOn' sep s = case s of { [] -> "" :| []; (part : remainder) -> if part == sep then "" <| splitOn' sep remainder else let head :| tail = splitOn' sep remainder in (part : head) :| tail}
+
+splitOn :: Char -> String -> [String]
+splitOn sep s = NE.toList (splitOn' sep s)
+
+-- Define input and output here
+type Input = (String, String)
+
+type Output = String
+
+formatOutput :: Output -> String
+formatOutput solution = solution
+
+parseInput :: String -> Input
+parseInput rawInput = let tmp = lines rawInput in (tmp !! 0, tmp !! 1)
+
+-- parseTestcases :: String -> [Input]
+-- parseTestcases rawInput = map parseInput tail (lines rawInput)
+
+solve :: Input -> Output
+solve (a, b) = if length a <= length b then "go" else "no"
