cfa1727f9a373ce4a9b982505b7672103b22d36d
Daniel Barenholz <hello@dbarenholz.com>
2025-02-17 14:32:16 +0100

    Add a_lead_i_bio.hs

---
+++ src/a_lead_i_bio/a_lead_i_bio.hs
@@ -0,0 +1,28 @@
+import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty ((:|)), (<|))
+import qualified Data.List.NonEmpty as NE
+
+main :: IO ()
+main = interact (formatOutput . solve . parseInput)
+
+-- helpers for parsing
+splitOn' :: Char -> String -> NonEmpty String
+splitOn' sep s = case s of { [] -> "" :| []; (part : remainder) -> if part == sep then "" <| splitOn' sep remainder else let head :| tail = splitOn' sep remainder in (part : head) :| tail}
+
+splitOn :: Char -> String -> [String]
+splitOn sep s = NE.toList (splitOn' sep s)
+
+-- Define input and output here
+type Input  = (Int, Int, Int)
+type Output = Int
+
+formatOutput :: Output -> String
+formatOutput solution = show solution  ++ "\n"
+
+parseInput :: String -> Input
+parseInput rawInput =
+  let l = lines rawInput
+  in (read $ l !! 0, read $ l !! 1, read $ l !! 2)
+
+solve :: Input -> Output
+solve (a, b, c) = c - (a + b)
