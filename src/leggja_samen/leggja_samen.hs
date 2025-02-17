219b1541f67008650db16a09f57d7672baaf6215
Daniel Barenholz <hello@dbarenholz.com>
2025-02-17 14:24:32 +0100

    Add leggja_samen.hs

---
+++ src/leggja_samen/leggja_samen.hs
@@ -0,0 +1,26 @@
+import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty ((:|)), (<|))
+import qualified Data.List.NonEmpty as NE
+
+main :: IO ()
+main = interact (formatOutput . solve . parseInput)
+
+-- helpers for parsing
+splitOn' :: Char -> String -> NonEmpty String
+splitOn' sep s = case s of { [] -> "" :| []; (part : remainder) -> if part == sep then "" <| splitOn' sep remainder else let head :| tail = splitOn' sep remainder in (part : head) :| tail}
+
+splitOn :: Char -> String -> [String]
+splitOn sep s = NE.toList (splitOn' sep s)
+
+-- Define input and output here
+type Input = [Int]
+type Output = Int
+
+formatOutput :: Output -> String
+formatOutput solution = show solution ++ "\n"
+
+parseInput :: String -> Input
+parseInput rawInput = map read $ lines rawInput
+
+solve :: Input -> Output
+solve = sum
