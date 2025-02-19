56d5669af32b5d709cc0a64e7e2efb8d69e9b0b7
Daniel Barenholz <hello@dbarenholz.com>
2025-02-19 16:12:02 +0100

    Add basketballoneonone.hs

---
+++ src/basketballoneonone/basketballoneonone.hs
@@ -0,0 +1,26 @@
+import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty ((:|)), (<|))
+import qualified Data.List.NonEmpty as NE
+
+main :: IO ()
+main = interact (formatOutput . solve . parseInput)
+
+-- helpers for parsing
+splitOn' :: Char -> String -> NonEmpty String
+splitOn' sep s = case s of { [] -> "" :| []; (part : remainder) -> if part == sep then "" <| splitOn' sep remainder else let head :| tail = splitOn' sep remainder in (part : head) :| tail}
+
+splitOn :: Char -> String -> [String]
+splitOn sep s = NE.toList (splitOn' sep s)
+
+-- Define input and output here
+type Input  = String
+type Output = Char
+
+formatOutput :: Output -> String
+formatOutput solution = solution : "\n"
+
+parseInput :: String -> Input
+parseInput rawInput = head $ lines rawInput
+
+solve :: Input -> Output
+solve line = reverse line !! 1
